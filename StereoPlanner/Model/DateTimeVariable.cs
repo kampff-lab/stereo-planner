f3e60b7362ff8f54436d6048a3b1885893357195
Joana <glopesdev@users.noreply.github.com>
2013-03-26 15:29:06 +0000

    Added Intervention property to stereotactic point to log time of intervention.

--- StereoPlanner/Model/DateTimeVariable.cs
+++ StereoPlanner/Model/DateTimeVariable.cs
@@ -13,28 +13,5 @@ namespace StereoPlanner.Model
     {
         [TypeConverter(typeof(NowDateTimeConverter))]
         public DateTime Value { get; set; }
-
-        class NowDateTimeConverter : DateTimeConverter
-        {
-            public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
-            {
-                return true;
-            }
-
-            public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
-            {
-                return new StandardValuesCollection(new[] { DateTime.Now });
-            }
-
-            public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
-            {
-                var text = value as string;
-                if (string.Compare(text, "now", true) == 0)
-                {
-                    return DateTime.Now;
-                }
-                return base.ConvertFrom(context, culture, value);
-            }
-        }
     }
 }
